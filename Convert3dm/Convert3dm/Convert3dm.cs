c119ce262ef4274641e381f8e8d16725f0795578
Luis Fraguada <luis@mcneel.com>
2020-11-11 15:11:42 +0100

    First commit

---
+++ Convert3dm/Convert3dm/Convert3dm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert3dm
+{
+    static class Convert3dm
+    {
+        public static string Convert3dmToFbx(string uri) 
+        {
+
+            //var doc = Rhino.FileIO.File3dm.Read(uri);
+
+            var doc = Rhino.RhinoDoc.Open(uri, out _);
+
+            string filename = @"E:\data\test.fbx";
+            string script = string.Format("_-SaveAs \"{0}\" _Enter _Enter", filename);
+
+            var result = Rhino.RhinoApp.RunScript(script, false);
+
+            return filename;
+        }
+    }
+}
---
+++ Convert3dm/Convert3dm/Convert3dmCommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace Convert3dm
+{
+    [Rhino.Commands.CommandStyle(Rhino.Commands.Style.ScriptRunner)]
+    public class Convert3dmCommand : Command
+    {
+        public Convert3dmCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static Convert3dmCommand Instance
+        {
+            get; private set;
+        }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName
+        {
+            get { return "Convert3dmCommand"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+
+            // select 3dm file
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "3dm files (*.3dm)|*.3dm;";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            string selectedFileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                selectedFileName = openFileDialog1.FileName;
+            }
+            else 
+            {
+                return Result.Failure;
+            }
+
+            var result = Convert3dm.Convert3dmToFbx(selectedFileName);
+
+            RhinoApp.WriteLine("Converted file: " + result);
+
+            return Result.Success;
+        }
+    }
+}
